4073f13c815263aa742b6c037468fcfb109d9ac3
PoonSlayer1337 <maxignatius02@gmail.com>
2019-01-22 08:43:21 +0100

    kms

--- www/asuslaptop.js
+++ www/asuslaptop.js
@@ -34,11 +34,11 @@ var i;
 for (i = 0; i < coll.length; i++) {
 coll[i].addEventListener("click", function() {
   this.classList.toggle("active");
-  var content = this.nextElementSibling;
-  if (content.style.maxHeight){
-    content.style.maxHeight = null;
+  var asuslaptopproduktspecifikationer = this.nextElementSibling;
+  if (asuslaptopproduktspecifikationer.style.maxHeight){
+    asuslaptopproduktspecifikationer.style.maxHeight = null;
   } else {
-    content.style.maxHeight = content.scrollHeight + "px";
+    asuslaptopproduktspecifikationer.style.maxHeight = asuslaptopproduktspecifikationer.scrollHeight + "px";
   }
 });
 }
